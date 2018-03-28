db609f0de6c07201be3d859970e1f8a1199a8b9a
guskant <gusni.kantu@gmail.com>
2018-03-28 09:40:00 +0000

    page for print

--- print.sh
+++ print.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-cd _tusin
-for i in *
-  do
-    sed \
-      -e 's/^layout: .*/layout: print/' \
-      $i > ../_p/$i
-  done
-cd -
+declare -a arr=("_tusin" "_info")
+for i in "${arr[@]}"
+do
+  cd $i
+    for j in *
+      do
+        sed \
+          -e 's/^layout: .*/layout: print/' \
+        $j > ../_p/$j
+      done
+  cd -
+done
+
