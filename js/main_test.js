02c734810764e603ccd16b222d6936f014ac0e18
guskant <gusni.kantu@gmail.com>
2018-04-12 03:41:31 +0000

    test js

--- js/main_test.js
+++ js/main_test.js
@@ -34,11 +34,11 @@ window.addEventListener('load', function (e) {
         if (supports_playback_rate) {
             var rate_range_element = document.getElementById('playback-rate');
             rate_range_element.disabled = false;
+            rate_range_element.addEventListener('keyup', function (e) {
+                args.audio_element.playbackRate = audio.playbackRate;
+            }, false);
             rate_range_element.addEventListener('change', function (e) {
                 args.audio_element.playbackRate = this.valueAsNumber;
-            }, false);
-            rate_range_element.addEventListener('keypress', function (e) {
-                args.audio_element.playbackRate = audio.playbackRate;
             }, false);
         }
         else {
