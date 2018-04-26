af3f8dd2a752897a8186b3119b60708f823cb65b
guskant <gusni.kantu@gmail.com>
2018-04-26 17:55:13 +0000

    logo

--- js/showtext.js
+++ js/showtext.js
@@ -4,14 +4,17 @@ function showtext() {
   // Get the output text
   var bun = document.getElementById("bun");
   var view = document.getElementById("view");
+  var logo = document.getElementById("logo");
 
   // If the checkbox is checked, display the output text
   if (checkBox.checked == true){
     bun.style.display = "block";
     view.style.display = "inline";
+    logo.style.display = "none";
   } else {
     bun.style.display = "none";
     view.style.display = "none";
+    logo.style.display = "block";
   }
 };
 
