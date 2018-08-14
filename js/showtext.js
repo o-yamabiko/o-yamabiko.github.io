56681d929494a46972f19ea64e72e55059700add
guskant <gusni.kantu@gmail.com>
2018-08-14 11:34:06 +0000

    test default layout

--- js/showtext.js
+++ js/showtext.js
@@ -5,16 +5,19 @@ function showtext() {
   var bun = document.getElementById("bun");
   var view = document.getElementById("view");
   var logo = document.getElementById("logo");
+  var chuu = document.getElementById("chuu");
 
   // If the checkbox is checked, display the output text
   if (checkBox.checked == true){
     bun.style.display = "block";
     view.style.display = "inline";
     logo.style.display = "none";
+    chuu.style.display = "none";
   } else {
     bun.style.display = "none";
     view.style.display = "none";
     logo.style.display = "block";
+    chuu.style.display = "inline";
   }
 };
 
