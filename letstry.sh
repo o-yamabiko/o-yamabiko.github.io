b010596b2a942f4f12bb7d03b20301256e8919ae
guskant <gusni.kantu@gmail.com>
2019-07-09 11:07:59 +0000

    tusin201907

--- letstry.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ruby at a character other than the beginning of a phrase should be prefixedby ｜.
-
-
-LC_COLLATE=C.UTF-8 sed \
-    -e 's/｜\([^(]*\) \(([^)]*)\) /<ruby>\1<rt>\2<\/rt><\/ruby>/g' \
-    -e 's/^\([^(]*\) \(([^)]*)\) /<ruby>\1<rt>\2<\/rt><\/ruby>/' \
-    $1 > $1.md
-
